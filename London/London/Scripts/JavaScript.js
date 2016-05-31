e4cd825a5655bb7b31412719a8e8775f908a679a
mixal <i.mixal@yandex.ru>
2016-05-31 23:41:14 +0300

    3 commit

--- London/London/Scripts/JavaScript.js
+++ London/London/Scripts/JavaScript.js
@@ -6,32 +6,45 @@ var map = new google.maps.Map(mapDiv, {
     zoom: 10,
 });
 function add_bikestation() {
-    $("#BikePoint").empty();
-    str.forEach(function (point) {
-        $("#BikePoint").append('<div class="row point"><ul><li class="point-li"><p>' + point.commonName + '</p></li><li class="point-li"><button commonName="' + point.commonName + '"  lat="' + point.lat + '" lon="' + point.lon + '" class="btn btn-success">Show on the map</button></li></ul></div>')
-        $(".btn").click(function () {
-            var myLatlng = new google.maps.LatLng($(this).attr("lat"), $(this).attr("lon"));
-            var mapOptions = {
-                zoom: 15,
-                center: myLatlng
-            }
-            var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-            var marker = new google.maps.Marker({
-                position: myLatlng,
-                title: $(this).attr("commonName")
+    if (str != []) {
+        $("#BikePoint").empty();
+        str.forEach(function (point) {
+            $("#BikePoint").append('<div class="row point"><ul><li class="point-li"><p>' + point.commonName + '</p></li><li class="point-li"><button commonName="' + point.commonName + '"  lat="' + point.lat + '" lon="' + point.lon + '" class="btn btn-success">Show on the map</button></li></ul></div>')
+            $(".btn").click(function () {
+                var myLatlng = new google.maps.LatLng($(this).attr("lat"), $(this).attr("lon"));
+                var mapOptions = {
+                    zoom: 15,
+                    center: myLatlng
+                }
+                var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+                var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    title: $(this).attr("commonName")
+                });
+                marker.setMap(map);
             });
-            marker.setMap(map);
         });
-    });
+    }
+    else {
+        alert("Nothing found");
+    }
 };
 function submit_form () {
     if ($("#search").val() != "") {
         $.get("https://api.tfl.gov.uk/BikePoint/Search?query=" + $("#search").val() + "&app_id=9732f448&app_key=1f73849015902fd185214be005f00ec9", function (data) {
-            str = data;
-            add_bikestation();
+            if (data != []) {
+                str = data;
+                add_bikestation();
+            }
+            else{
+                alert("Nothing found");
+            }
         });
-
+    }
+    else {
+        alert("Nothing found");
     }
    
 };
